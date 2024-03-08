8e41d96d8f688e0b9d5db40574c17fb8833bfc9d
Raafina <raafinuradzani@gmail.com>
2024-03-08 20:07:49 +0700

    first commit

---
+++ scripts/cars.js
@@ -0,0 +1,34 @@
+import carsData from "./data/cars.json" assert { type: "json" };
+
+class Cars {
+  getAllCars() {
+    return carsData;
+  }
+
+  filterCarAvailable() {
+    const carsAvailable = [];
+    carsData.map((car) => {
+      if (car.available == true) {
+        carsAvailable.push(car);
+      }
+    });
+    return carsAvailable;
+  }
+
+  carsSearch(date, totalPassanger) {
+    const carAvailable = this.filterCarAvailable();
+    const carsSearched = carAvailable.filter(
+      (car) =>
+        new Date(car.availableAt.substring(0, 10)) <= new Date(date) &&
+        totalPassanger <= car.capacity
+    );
+    return carsSearched;
+  }
+
+  carsSortByYearAscending(arr) {
+    arr.sort((car1, car2) => car1.year - car2.year);
+    return arr;
+  }
+}
+
+export default new Cars();
---
+++ scripts/index.js
@@ -0,0 +1,65 @@
+import cars from "./cars.js";
+
+const cardCar = document.getElementById("card-car");
+const dateRent = document.getElementById("date");
+const totalPassanger = document.getElementById("total-passanger");
+const buttonSearchCar = document.getElementById("search-submit");
+
+buttonSearchCar.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let printSearchCarCard = "";
+  cars.carsSearch(dateRent.value, totalPassanger.value).map((car) => {
+    printSearchCarCard += `<div class="col-md-4 p-1 d-flex">
+                <div class="card flex-fill">
+                  <img
+                    src="${car.image}"
+                    class="card-img-top p-4"
+                    alt="car sample"
+                  />
+                  <div class="card-body d-flex flex-column">
+                    <p style="font-weight: 500">${car.model}</p>
+                    <h5 class="card-title">${car.rentPerDay}</h5>
+                    <p class="card-text">
+                      ${car.description}
+                    </p>
+                    <p>
+                      <span
+                        ><img
+                          src="Public/Icons/icon_jumlahOrang.png"
+                          alt="icon jumlah orang"
+                      /></span>
+                       ${car.capacity} Orang
+                    </p>
+                    <p>
+                      <span
+                        ><img
+                          src="Public/Icons/icon_Spesifikasi.png"
+                          alt="icon spesifikasi"
+                      /></span>
+                      ${car.transmission}
+                    </p>
+                    <p>
+                      <span
+                        ><img src="Public/Icons/icon_date.png" alt="icon date"
+                      /></span>
+                      Tahun ${car.year}
+                    </p>
+                    <div class="mt-auto">
+                    <a
+                      href="#"
+                      class="btn btn-success d-flex flex-fill justify-content-center"
+                      style="
+                        background-color: #5cb85f;
+                        font-weight: 500;
+                        color: white;
+                      "
+                      >Pilih Mobil</a
+                    >
+                    </div>
+                  </div>
+                </div>
+              </div>`;
+  });
+  cardCar.innerHTML = printSearchCarCard;
+});
