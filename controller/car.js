bbe9d0fb69c1f6fa6077fdd07531980a744748bf
Raafina <raafinuradzani@gmail.com>
2024-03-19 23:41:10 +0700

    final-commit

--- controller/car.js
+++ controller/car.js
@@ -20,7 +20,7 @@ exports.getCar = (req, res) => {
 
 exports.addCar = (req, res) => {
   const data = carUseCase.addCar(req);
-
+  validatorInput(req, res);
   const response = {
     data,
     message: "Data berhasil ditambahkan",
@@ -30,7 +30,7 @@ exports.addCar = (req, res) => {
 
 exports.updateCar = (req, res) => {
   const data = carUseCase.updateCar(req);
-
+  validatorInput(req, res);
   const response = {
     data,
     message: "Data berhasil diupdate",
@@ -48,4 +48,100 @@ exports.deleteCar = (req, res) => {
   res.status(201).json(response);
 };
 
+validatorInput = (req, res) => {
+  const {
+    plate,
+    manufacture,
+    model,
+    image,
+    rentPerDay,
+    capacity,
+    description,
+    availableAt,
+    transmission,
+    available,
+    type,
+    year,
+    options,
+    specs,
+  } = req.body;
 
+  if (!plate || plate == "") {
+    return res.status(400).json({
+      data: null,
+      message: "plate must be filled!",
+    });
+  }
+  if (!manufacture || manufacture == "") {
+    return res.status(400).json({
+      data: null,
+      message: "manufacture must be filled!",
+    });
+  }
+  if (!model || model == "") {
+    return res.status(400).json({
+      data: null,
+      message: "model must be filled!",
+    });
+  }
+  if (!rentPerDay || rentPerDay == "") {
+    return res.status(400).json({
+      data: null,
+      message: "rentPerDay must be filled!",
+    });
+  }
+  if (!capacity || capacity == "") {
+    return res.status(400).json({
+      data: null,
+      message: "capacity must be filled!",
+    });
+  }
+  if (!description || description == "") {
+    return res.status(400).json({
+      data: null,
+      message: "description must be filled!",
+    });
+  }
+  if (!availableAt || availableAt == "") {
+    return res.status(400).json({
+      data: null,
+      message: "availableAt must be filled!",
+    });
+  }
+  if (!transmission || transmission == "") {
+    return res.status(400).json({
+      data: null,
+      message: "transmission must be filled!",
+    });
+  }
+  if (!available || available == "") {
+    return res.status(400).json({
+      data: null,
+      message: "available must be filled!",
+    });
+  }
+  if (!type || type == "") {
+    return res.status(400).json({
+      data: null,
+      message: "type must be filled!",
+    });
+  }
+  if (!year || year == "") {
+    return res.status(400).json({
+      data: null,
+      message: "year must be filled!",
+    });
+  }
+  if (!options || options == "") {
+    return res.status(400).json({
+      data: null,
+      message: "options must be filled!",
+    });
+  }
+  if (!specs || specs == "") {
+    return res.status(400).json({
+      data: null,
+      message: "specs must be filled!",
+    });
+  }
+};
